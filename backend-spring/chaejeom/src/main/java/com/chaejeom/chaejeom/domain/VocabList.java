c3a58f571a407850b4628aa2d0f811280974ba6d
wooocha <csw0512@naver.com>
2023-11-03 16:36:03 +0900

    #14 단어장 도메인 구성

---
+++ backend-spring/chaejeom/src/main/java/com/chaejeom/chaejeom/domain/VocabList.java
@@ -0,0 +1,39 @@
+package com.chaejeom.chaejeom.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "voca_list")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@AllArgsConstructor
+public class VocabList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "voca_list_id")
+    private Long id;
+
+    @Column(nullable = false, name = "voca_list_name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "vocabList")
+    private List<VocabListContent>  vocabListContents = new ArrayList<VocabListContent>();
+
+
+}
