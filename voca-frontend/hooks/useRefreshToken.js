1fb06e8c6920fe9720cdf84fe20699151c370d58
HI <mash001004@naver.com>
2023-11-11 22:28:55 +0900

    Added hooks : Implemented JWT Login

---
+++ voca-frontend/hooks/useRefreshToken.js
@@ -0,0 +1,25 @@
+import axios from "../api/axios";
+import useAuth from "./useAuth";
+
+const useRefreshToken = () => {
+  const { setAuth } = useAuth();
+
+  const refresh = async () => {
+    try {
+      const response = await axios.post("/auth/reissue", {
+        withCredentials: true,
+      });
+      setAuth((prev) => {
+        console.log("PREVIOUS REQUEST?");
+        console.log(JSON.stringify(prev));
+        return { ...prev, accessToken: response.data.accessToken };
+      });
+      return response.data.accessToken;
+    } catch (err) {
+      console.log("UNEXPECTED ERROR");
+    }
+  };
+  return refresh;
+};
+
+export default useRefreshToken;
