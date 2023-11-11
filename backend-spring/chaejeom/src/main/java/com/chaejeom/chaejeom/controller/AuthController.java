7a8d543c678bbfcaabaad2ec948b43ece85b5c38
wooocha <csw0512@naver.com>
2023-11-11 18:32:07 +0900

    #12 refresh 토큰 httponly 쿠키로 전달하게 수정

--- backend-spring/chaejeom/src/main/java/com/chaejeom/chaejeom/controller/AuthController.java
+++ backend-spring/chaejeom/src/main/java/com/chaejeom/chaejeom/controller/AuthController.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -26,9 +28,17 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
-
-        return ResponseEntity.ok(authService.login(memberRequestDto));
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto, HttpServletResponse response) {
+        TokenDto tokenDto = authService.login(memberRequestDto);
+        ResponseCookie responseCookie = ResponseCookie.from("refreshToken", tokenDto.getRefreshToken())
+                .httpOnly(true)
+                //.sameSite("None")
+                // secure 를 true로 설정하면 https 에서만 쿠키가 전달됨 추후 적용 후 수정필요.
+                .secure(false)
+                .path("/")
+                .maxAge( 60 * 60 * 24 * 7)
+                .build();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, responseCookie.toString()).body(tokenDto);
     }
 
     @Operation(
