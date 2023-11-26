36932b3cfcb54ec797411dde06d8f5d557b750a7
HI <mash001004@naver.com>
2023-11-26 18:50:15 +0900

    Updated useAxiosPrivate.js : reissue condition update

--- voca-frontend/src/hooks/useAxiosPrivate.js
+++ voca-frontend/src/hooks/useAxiosPrivate.js
@@ -12,7 +12,12 @@ const useAxiosPrivate = () => {
       (response) => response,
       async (error) => {
         const prevRequest = error?.config;
-        if (error?.response?.status === 403 && !prevRequest?.sent) {
+        if (
+          error?.response?.status === 401 &&
+          error?.response?.data.code === "TOKEN-002" &&
+          !prevRequest?.sent
+        ) {
+          console.log(error?.response?.data.code);
           prevRequest.sent = true;
           const newAccessToken = await refresh();
           prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
--- voca-frontend/src/hooks/useRefreshToken.js
+++ voca-frontend/src/hooks/useRefreshToken.js
@@ -1,4 +1,4 @@
-import axios from "../api/axios";
+import axios, { axiosPrivate } from "../api/axios";
 import useAuth from "./useAuth";
 
 const useRefreshToken = () => {
@@ -6,7 +6,7 @@ const useRefreshToken = () => {
 
   const refresh = async () => {
     try {
-      const response = await axios.post("/auth/reissue", {
+      const response = await axiosPrivate.post("/auth/reissue", {
         withCredentials: true,
       });
       setAuth((prev) => {
@@ -16,6 +16,7 @@ const useRefreshToken = () => {
       });
       return response.data.accessToken;
     } catch (err) {
+      //console.log(err);
       console.log("UNEXPECTED ERROR");
     }
   };
